9f49649b29d3541572ced9cc4727dc948e3719d0
Kamil Szpak <dev@kamilszpak.com>
2026-03-30 14:37:36 +0200

    fix: Fixed ui testing project app

---
+++ HarvestTester/HarvestWatchTester Watch App/HarvestWatchTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarvestWatchTesterApp.swift
+//  HarvestWatchTester Watch App
+//
+//  Created by Kamil Szpak on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HarvestWatchTester_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
